7f4e5272faa660fe82a365c1b3e1684ca57526d0
nlf901 <47324430+nlf901@users.noreply.github.com>
2019-04-26 16:33:07 -0400

    Create testcode.py

---
+++ test/testcode.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""
+This module is to run simplest pygame-test for the project
+"""
+import sys
+import pygame
+# Import additional modules here.
+
+
+# Feel free to edit these constants to suit your requirements.
+FRAME_RATE = 60
+SCREEN_SIZE = (1200, 800)
+
+
+def pygame_modules_have_loaded():
+    success = True
+
+    if not pygame.display.get_init:
+        success = False
+    if not pygame.font.get_init():
+        success = False
+    if not pygame.mixer.get_init():
+        success = False
+
+    return success
+
+pygame.mixer.pre_init(44100, -16, 2, 512)
+pygame.init()
+pygame.font.init()
+
+if pygame_modules_have_loaded():
+    game_screen = pygame.display.set_mode(SCREEN_SIZE)
+    pygame.display.set_caption('Test')
+    clock = pygame.time.Clock()
+
+    def declare_globals():
+        pass
+
+    def prepare_test():
+        pass
+
+    def handle_input(key_name):
+        pass
+
+    def update(screen, time):
+        pygame.display.update()
+        
+    def main():
+        declare_globals()
+        prepare_test()
+
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+                if event.type == pygame.KEYDOWN:
+                    key_name = pygame.key.name(event.key)
+                    handle_input(key_name)
+
+            milliseconds = clock.tick(FRAME_RATE)
+            seconds = milliseconds / 1000.0
+            update(game_screen, seconds)
+
+            sleep_time = (1000.0 / FRAME_RATE) - milliseconds
+            if sleep_time > 0.0:
+                pygame.time.wait(int(sleep_time))
+            else:
+                pygame.time.wait(1)
+
+    main()
+
